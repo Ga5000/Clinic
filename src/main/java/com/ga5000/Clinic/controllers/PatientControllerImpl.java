0d731ed97642414024b523d7290c770fafa4b9e2
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-09-11 20:17:56 -0300

    new methods

--- src/main/java/com/ga5000/Clinic/controllers/PatientControllerImpl.java
+++ src/main/java/com/ga5000/Clinic/controllers/PatientControllerImpl.java
@@ -1,6 +1,35 @@
 package com.ga5000.Clinic.controllers;
 
 import com.ga5000.Clinic.controllers.interfaces.PatientController;
+import com.ga5000.Clinic.dtos.Appointment.AppointmentDTO;
+import com.ga5000.Clinic.services.PatientServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/patient")
 public class PatientControllerImpl implements PatientController {
+
+    private final PatientServiceImpl patientService;
+
+    public PatientControllerImpl(PatientServiceImpl patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/scheduled-appointments/{patientId}")
+    @Override
+    public ResponseEntity<List<AppointmentDTO>> getScheduledAppointments(@PathVariable("patientId") Long patientId) {
+        return ResponseEntity.status(HttpStatus.OK).body(patientService.getAllScheduledAppointments(patientId));
+    }
+
+    @Override
+    public ResponseEntity<List<AppointmentDTO>> getAppointmentHistory(@PathVariable("patientId") Long patientId) {
+        return ResponseEntity.status(HttpStatus.OK).body(patientService.getAppointmentHistory(patientId));
+    }
 }
--- src/main/java/com/ga5000/Clinic/controllers/interfaces/PatientController.java
+++ src/main/java/com/ga5000/Clinic/controllers/interfaces/PatientController.java
@@ -1,4 +1,10 @@
 package com.ga5000.Clinic.controllers.interfaces;
+import com.ga5000.Clinic.dtos.Appointment.AppointmentDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface PatientController {
+    ResponseEntity<List<AppointmentDTO>> getScheduledAppointments(Long patientId);
+    ResponseEntity<List<AppointmentDTO>> getAppointmentHistory(Long patientId);
 }
